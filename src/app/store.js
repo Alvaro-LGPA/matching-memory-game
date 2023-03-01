d576e2dfabd4e3a5cac22813e9036bc14d2e2ead
Alvaro-LGPA <alvarolaura@gmail.com>
2023-03-01 17:44:57 +0100

    Refactor to use configureStore

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,6 @@
-import { combineReducers, createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 
 import { boardReducer } from '../features/board/boardSlice.js';
 
-const rootReducer = combineReducers({
-  board: boardReducer,
-});
 
-export const store = createStore(rootReducer);
+export const store = configureStore({reducer: boardReducer});
