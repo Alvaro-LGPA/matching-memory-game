5d492b5d33b83babaaf4552be44698d08247371d
Alvaro-LGPA <alvarolaura@gmail.com>
2023-03-02 12:35:42 +0100

    Add imports

--- src/features/board/cardRow/card/Card.js
+++ src/features/board/cardRow/card/Card.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // Add import statements below
-import { useSelector } from 'react-redux';
-import { selectVisibleIDs } from '../../boardSlice';
+import { useSelector, dispatch } from 'react-redux';
+import { selectVisibleIDs, flipCard } from '../../boardSlice';
 
 let cardLogo = "https://static-assets.codecademy.com/Courses/Learn-Redux/matching-game/codecademy_logo.png";
 
