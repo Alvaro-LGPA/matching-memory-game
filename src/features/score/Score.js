2e4d704b1a4b2996c694c0e045eda99da6e12f99
Alvaro-LGPA <alvarolaura@gmail.com>
2023-03-02 13:00:10 +0100

    Asign selector variable and replace hard coded value with variable

--- src/features/score/Score.js
+++ src/features/score/Score.js
@@ -5,10 +5,10 @@ import { selectMatchedIDs } from '../board/boardSlice';
 
 export const Score = () => {
   // Add selected data variable below
-  
+  const cardsMatched = useSelector(selectMatchedIDs)
 
   return (
     // implement selected data inside <div>
-    <div className="score-container">Matched: 0</div>
+    <div className="score-container">Matched: {cardsMatched.length}</div>
   );
 };
